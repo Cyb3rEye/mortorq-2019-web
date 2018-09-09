49dc9e2a5d45e9b61885b55f71071b178113f1b3
Cyb3rEye <JaydenB0@pm.me>
2018-09-09 10:25:33 -0700

    Replaced placeholders with actual links; made static files local

--- src/start.py
+++ src/start.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__, template_folder="pages")
-
+app._static_folder = "pages/static"
 @app.route('/')
 def index():
     return render_template('index.html')
