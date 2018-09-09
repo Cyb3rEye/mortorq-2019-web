7e472ed4c3ada382fb71fedb458e9af293d9f58e
Jayden Bulexa <JaydenB0@pm.me>
2018-09-09 13:24:38 -0700

    Added active highlights for links; About page

--- src/start.py
+++ src/start.py
@@ -4,4 +4,9 @@ app._static_folder = "pages/static"
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
 app.run()
