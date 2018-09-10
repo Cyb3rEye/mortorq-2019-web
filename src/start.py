7cb88bd51aba88b48cb3ee9837f132b667ce044c
Cyb3rEye <JaydenB0@pm.me>
2018-09-10 11:07:07 -0700

    __main__ check and sponsor route added

--- src/start.py
+++ src/start.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__, template_folder="pages")
 app._static_folder = "pages/static"
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -12,5 +13,8 @@ def about():
 @app.route('/awards/')
 def awards():
     return render_template('awards.html')
-
-app.run()
+@app.route('/sponsors/')
+def sponsors():
+    return render_template('sponsors.html')
+if __name__ == '__main__':
+    app.run()
